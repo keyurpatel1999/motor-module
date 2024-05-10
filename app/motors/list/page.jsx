1cbc1e63d0d763cc9622614021dc05bdc2aa3afa
JaykrushnDhameliya <jkdhameliya276@gmail.com>
2024-05-10 14:16:35 +0530

    Changes

--- app/motors/list/page.jsx
+++ app/motors/list/page.jsx
@@ -1,5 +1,55 @@
 import Image from "next/image";
 
+const motorFilters = [
+  {
+    name: "Used Cars",
+    quantity: 132,
+  },
+  {
+    name: "Number Plates",
+    quantity: 132,
+  },
+  {
+    name: "Auto Accessories & Parts",
+    quantity: 132,
+  },
+  {
+    name: "Motor Cycles",
+    quantity: 132,
+  },
+  {
+    name: "Heavy Vehicles",
+    quantity: 132,
+  },
+  {
+    name: "Boats",
+    quantity: 132,
+  },
+  {
+    name: "Export Cars",
+    quantity: 132,
+  },
+];
+
+const whatIsNew = [
+  {
+    title: "<b>MJ Cars</b> is listed a 20 new cars and bikes",
+    imageUrl: "/mj-car.png",
+  },
+  {
+    title: "<b>Crystocab</b> is a listed a Hyundai branded used cars",
+    imageUrl: "/crystocab.jpg",
+  },
+  {
+    title: "<b>Dravien Desel</b> is a listed a Rehana branded used cars",
+    imageUrl: "/dravien.jpg",
+  },
+  {
+    title: "<b>Maximo Cars</b> is a listed a Maruthi Xylo branded used cars",
+    imageUrl: "/maximo.jpg",
+  },
+];
+
 const MotorListPage = () => {
   return (
     <div className="px-[100px]">
@@ -99,10 +149,91 @@ const MotorListPage = () => {
         </div>
       </div>
 
-      <div className="flex">
+      <div className="flex gap-x-[20px]">
+        {motorFilters.map((filter, index) => {
+          return (
+            <div
+              key={index}
+              className="px-[20px] h-[41px] flex items-center border-solid border-[1px] border-[#D9D9D9] rounded-[23px] cursor-pointer"
+            >
+              <div className="text-black text-[13px]">
+                <span>{filter.name}</span>{" "}
+                <span className="text-[#4B4CFF]">({filter.quantity})</span>
+              </div>
+            </div>
+          );
+        })}
+
         <div className="px-[20px] h-[41px] flex items-center border-solid border-[1px] border-[#D9D9D9] rounded-[23px] cursor-pointer">
-          <div className="text-black text-[13px]">
-            Used Cars <span className="text-[#4B4CFF]">(132)</span>
+          <div className="text-[#F800C0] text-[13px]">More</div>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex flex-col w-[400px] h-[228px] gap-y-[20px]">
+          <Image
+            className="cursor-pointer"
+            src={"/youtube-ad.png"}
+            width={400}
+            height={228}
+            alt="Youtube Ad"
+          />
+
+          <Image
+            className="cursor-pointer h-[482px]"
+            src={"/signup-now.png"}
+            width={400}
+            height={482}
+            alt="Signup Now"
+          />
+
+          <Image
+            className="cursor-pointer h-[479px]"
+            src={"/advertising.png"}
+            width={400}
+            height={479}
+            alt="Advertising"
+          />
+
+          <div className="w-full flex flex-col gap-y-[20px] bg-[#F8F8F8] border-[1px] border-solid border-[#EDEDED] px-[12px] pt-[20px] pb-[10px] rounded-[19px]">
+            <div className="text-[#4B4CFF] font-semibold text-[25px]">
+              What's New
+            </div>
+
+            {whatIsNew.map((item, index) => {
+              return (
+                <div className="flex gap-x-[12px] items-center">
+                  <Image
+                    className="rounded-full"
+                    src={item.imageUrl}
+                    width={70}
+                    height={70}
+                    alt="MJ Cars"
+                  />
+
+                  <div
+                    className="text-black text-[18px] leading-8"
+                    dangerouslySetInnerHTML={{
+                      __html: item.title,
+                    }}
+                  ></div>
+                </div>
+              );
+            })}
+
+            <button className="min-h-[56px] flex justify-between items-center border border-solid border-[#003DCA] pl-[25px] pr-[6px] rounded-[20px]">
+              <div className="text-[21px]">View All</div>
+
+              <button className="w-[68px] h-[44px] bg-[#4B4CFF] flex justify-center items-center rounded-[21px]">
+                <Image
+                  className="cursor-pointer h-[44px]"
+                  src={"/arrow-right.svg"}
+                  width={21}
+                  height={15}
+                  alt="Advertising"
+                />
+              </button>
+            </button>
           </div>
         </div>
       </div>
